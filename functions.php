7684933ac1b07eb67fb57718c891953b6926005c
jeremyfelt <jeremy.felt@gmail.com>
2014-08-19 09:22:21 -0700

    Remove enqueue of empty custom Javascript file

--- functions.php
+++ functions.php
@@ -2,12 +2,3 @@
 
 // Include functionality for handling degrees
 include_once( __DIR__ . '/includes/class-grad-degrees.php' );
-
-add_action( 'wp_enqueue_scripts', 'gradschool_enqueue_scripts' );
-/**
- * Enqueue the script required by the Grad School theme.
- */
-function gradschool_enqueue_scripts() {
-	// Script only contains document.ready calls, load in the footer.
-	wp_enqueue_script( 'gradschool-script', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), spine_get_script_version(), true );
-}

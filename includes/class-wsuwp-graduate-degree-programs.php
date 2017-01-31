890e8731e9c735c88b86525eb08f34a31121f87c
Jeremy Felt <jeremy.felt@gmail.com>
2017-01-31 15:16:49 -0800

    Use degrees-beta for the new degrees interface

--- includes/class-wsuwp-graduate-degree-programs.php
+++ includes/class-wsuwp-graduate-degree-programs.php
@@ -193,8 +193,8 @@ class WSUWP_Graduate_Degree_Programs {
 			'supports' => array(
 				'title',
 			),
-			'has_archive' => 'degrees',
-			'rewrite' => array( 'slug' => 'degrees/factsheet', 'with_front' => false ),
+			'has_archive' => 'degrees-beta',
+			'rewrite' => array( 'slug' => 'degrees-beta/factsheet', 'with_front' => false ),
 		);
 		register_post_type( $this->post_type_slug, $args );
 		register_taxonomy_for_object_type( 'wsuwp_university_location', $this->post_type_slug );

fd5397283426827bb2faa55a440a3bfcbd988f38
Jeremy Felt <jeremy.felt@gmail.com>
2017-02-16 12:35:10 -0800

    Allow capture of degree classification on degree types

--- includes/class-graduate-degree-degree-type-taxonomy.php
+++ includes/class-graduate-degree-degree-type-taxonomy.php
@@ -18,6 +18,21 @@ class WSUWP_Graduate_Degree_Degree_Type_Taxonomy {
 	var $taxonomy_slug = 'gs-degree-type';
 
 	/**
+	 * A list of term meta keys associated with degree types.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @var array
+	 */
+	var $term_meta_keys = array(
+		'gs_degree_type_classification' => array(
+			'description' => 'Degree Classification',
+			'type' => 'string',
+			'sanitize_callback' => 'WSUWP_Graduate_Degree_Degree_Type_Taxonomy::sanitize_degree_classification',
+		),
+	);
+
+	/**
 	 * Maintain and return the one instance. Initiate hooks when
 	 * called the first time.
 	 *
@@ -40,6 +55,9 @@ class WSUWP_Graduate_Degree_Degree_Type_Taxonomy {
 	 */
 	public function setup_hooks() {
 		add_action( 'init', array( $this, 'register_taxonomy' ), 20 );
+		add_action( 'init', array( $this, 'register_meta' ), 25 );
+		add_action( "{$this->taxonomy_slug}_edit_form_fields", array( $this, 'term_edit_form_fields' ), 10 );
+		add_action( "edit_{$this->taxonomy_slug}", array( $this, 'save_term_form_fields' ) );
 	}
 
 	/**
@@ -70,5 +88,124 @@ class WSUWP_Graduate_Degree_Degree_Type_Taxonomy {
 			'rewrite'           => false,
 		);
 		register_taxonomy( $this->taxonomy_slug, array( WSUWP_Graduate_Degree_Programs()->post_type_slug ), $args );
+	}
+
+
+	/**
+	 * Registers the meta keys used with degree type data.
+	 *
+	 * @since 0.8.0
+	 */
+	public function register_meta() {
+		foreach ( $this->term_meta_keys as $key => $args ) {
+			$args['show_in_rest'] = true;
+			$args['single'] = true;
+
+			register_meta( 'term', $key, $args );
+		}
+	}
+
+	/**
+	 * Saves the additional form fields whenever a term is updated.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @param int $term_id The ID of the term being edited.
+	 */
+	public function save_term_form_fields( $term_id ) {
+		global $wp_list_table;
+
+		if ( 'editedtag' !== $wp_list_table->current_action() ) {
+			return;
+		}
+
+		// Reuse the default nonce that is checked in `edit-tags.php`.
+		check_admin_referer( 'update-tag_' . $term_id );
+
+		$keys = get_registered_meta_keys( 'term' );
+
+		foreach ( $this->term_meta_keys as $key => $meta ) {
+			if ( isset( $_POST[ $key ] ) && isset( $keys[ $key ] ) && isset( $keys[ $key ]['sanitize_callback'] ) ) {
+				// Each piece of meta is registered with sanitization.
+				update_term_meta( $term_id, $key, $_POST[ $key ] );
+			}
+		}
+
+		return;
+	}
+
+	/**
+	 * Retrieves all of the expected degree type meta assigned to a term.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @param int $term_id
+	 *
+	 * @return array
+	 */
+	public static function get_all_term_meta( $term_id ) {
+		$term_meta = get_registered_metadata( 'term', $term_id );
+
+		return $term_meta;
+	}
+
+	/**
+	 * Captures information about a degree type as term meta.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @param WP_Term $term
+	 */
+	public function term_edit_form_fields( $term ) {
+		$term_meta = self::get_all_term_meta( $term->term_id );
+
+		foreach( $this->term_meta_keys as $key => $meta ) {
+			if ( 'gs_degree_type_classification' !== $key ) {
+				continue;
+			}
+
+			if ( isset( $term_meta[ $key ][0] ) ) {
+				$selected_value = $term_meta[ $key ][0];
+			} else {
+				$selected_value = 'Other';
+			}
+
+			?>
+			<tr class="form-field">
+				<th scope="row">
+					<label for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $meta['description'] ); ?></label>
+				</th>
+				<td>
+					<select name="<?php echo esc_attr( $key ); ?>" id="<?php echo esc_attr( $key ); ?>">
+						<option value="Other" <?php selected( 'None', $selected_value ); ?>>Other</option>
+						<option value="Doctorate" <?php selected( 'Doctorate', $selected_value ); ?>>Doctorate</option>
+						<option value="Masters" <?php selected( 'Masters', $selected_value ); ?>>Masters</option>
+						<option value="Graduate Certificate" <?php selected( 'Graduate Certificate', $selected_value ); ?>>Graduate Certificate</option>
+					</select>
+				</td>
+			</tr>
+			<?php
+		}
+	}
+
+	/**
+	 * Sanitizes a classification for a degree type.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @param string $classification
+	 *
+	 * @return string
+	 */
+	public static function sanitize_degree_classification( $classification ) {
+		if ( empty( $classification ) ) {
+			$classification = 'Other';
+		}
+
+		if ( ! in_array( $classification, array( 'None', 'Masters', 'Doctorate', 'Graduate Certificate', 'Other' ), true ) ) {
+			$classification = 'Other';
+		}
+
+		return $classification;
 	}
 }
